f7d17c26944646fc7497e81b86e8887556e4b2ed
volokhviktor <volohviktor1@gmail.com>
2026-07-16 20:25:01 +0200

    Init Avalonia project

---
+++ Unidictionary/Unidictionary/Views/MainView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia;
+using Avalonia.Controls;
+
+namespace Unidictionary.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
